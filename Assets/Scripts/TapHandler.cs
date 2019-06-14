5d7fff94f0ab706b1350703b3a4316fa40f0e9e9
richard.kiss <richard.kiss@suitsolutions.eu>
2019-06-14 11:21:13 +0200

    +function that allows you to rotate camera with tap input added to taphandler.cs

--- Assets/Scripts/TapHandler.cs
+++ Assets/Scripts/TapHandler.cs
@@ -11,21 +11,43 @@ using System.Collections;
 
 public class TapHandler : MonoBehaviour
 {
+    private Touch inittouch = new Touch();
+    public Camera cam;
+    [SerializeField] private float rotSpeed = 0.5f, dir = -1;
+    private float rotX = 0f, rotY = 0;
+    private Vector3 originRot;
 
-    void Start()
+    private void Start()
     {
-       
+        originRot = cam.transform.eulerAngles;
+        rotX = originRot.x;
+        rotY = originRot.y;
     }
 
     void Update()
     {
         // On Android, the Back button is mapped to the Esc key
-		if (Application.platform == RuntimePlatform.Android)
-		{
-			if (Input.GetKeyUp(KeyCode.Escape))
-			{
-				Application.Quit();
-			}
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began) inittouch = touch;
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    float deltaX = inittouch.position.x - touch.position.x;
+                    float deltaY = inittouch.position.y - touch.position.y;
+                    rotX -= deltaX * Time.deltaTime * rotSpeed * dir;
+                    rotY -= deltaY * Time.deltaTime * rotSpeed * dir;
+                    rotX =Mathf.Clamp(rotX, -80f, 80f);
+                    cam.transform.eulerAngles = new Vector3(rotX, rotY, 0f);          
+                }
+                else if (touch.phase == TouchPhase.Ended) inittouch = new Touch();
+            }
 		}
 
     }
